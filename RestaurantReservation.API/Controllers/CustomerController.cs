b712ba1c1ff0602b4ce6fe3510467487e073c171
Jana Saa <jana.abusaa@gmail.com>
2024-11-16 21:37:27 +0200

    Create customer controller

--- RestaurantReservation.API/Controllers/CustomerController.cs
+++ RestaurantReservation.API/Controllers/CustomerController.cs
@@ -1,13 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Interfaces;
 using RestaurantReservation.Db.DataModels;
-using RestaurantReservation.Db;
 
 namespace RestaurantReservation.API.Controllers
 {
     [ApiController]
     [Route("api/customer")]
-    public class CustomerController(RestaurantReservationDbContext context) : Controller
+    public class CustomerController(ICustomerService customerService) : Controller
     {
-        private readonly RestaurantReservationDbContext _context = context;
+        private readonly ICustomerService _customerService = customerService;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            var customers = await _customerService.GetAllCustomersAsync();
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            var customer = await _customerService.GetCustomerByIdAsync(id);
+            if (customer == null)
+            {
+                return NotFound(new { Message = "Invalid Id!" });
+            }
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateCustomer(Customer customer)
+        {
+            await _customerService.CreateCustomerAsync(customer);
+            return CreatedAtAction(nameof(GetCustomer), new { id = customer.CustomerId }, customer);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCustomer(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return BadRequest(new { Message = "Invalid Id!" });
+            }
+
+            await _customerService.UpdateCustomerAsync(customer);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            await _customerService.DeleteCustomerAsync(id);
+            return NoContent();
+        }
     }
 }
--- RestaurantReservation.API/Program.cs
+++ RestaurantReservation.API/Program.cs
@@ -1,15 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+using RestaurantReservation.Db;
+using RestaurantReservation.API.Interfaces;
+using RestaurantReservation.API.Services;
+using RestaurantReservation.API.Repositories;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+var config = new ConfigurationBuilder().SetBasePath(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\")))
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+builder.Services.AddDbContext<RestaurantReservationDbContext>(options => options.UseSqlServer(config.GetConnectionString("RestaurantReservationCore")));
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
